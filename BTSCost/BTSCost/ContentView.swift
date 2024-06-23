02a1ac3dcd6c8018a0e89bbb87d0e8cd914e1619
Waterforces <131222639+Waterforces@users.noreply.github.com>
2024-06-23 01:12:08 -0700

    Commits 11

--- BTSCost/BTSCost/ContentView.swift
+++ BTSCost/BTSCost/ContentView.swift
@@ -14,41 +14,52 @@ struct ContentView: View {
 //    @State var e:Int = 0
     @State var showResult: Bool=false
     var body: some View {
-        Spacer()
-        VStack {
-            Text("How to Go")
-                .font(.title)
-                .fontWeight(.bold)
-            Image("Image")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 100,height: 100)
-        }
         ZStack {
-            RoundedRectangle(cornerRadius: 20.0)
-                .fill(.gray)
-                .opacity(0.3)
-                .aspectRatio(contentMode: .fit)
+            Color.yellow
+                .ignoresSafeArea(edges: .all)
+                .opacity(0.5)
+            Spacer()
             VStack {
-                Text("สถานีเริ่มต้น")
-                    .font(.title)
-                    .fontWeight(.bold)
-                TextField("ถานีเริ่มต้น", value: $start, formatter: NumberFormatter())
-                    .textFieldStyle(.roundedBorder)
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200)
-                Text("สถานที่ปลายทาง")
-                    .font(.title)
-                    .fontWeight(.bold)
-                TextField("สถานที่ปลายทาง", value: $end, formatter: NumberFormatter())
-                    .textFieldStyle(.roundedBorder)
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200)
-                Button("Submit",action:{showResult.toggle()} )
-                    .padding([.top],40)
-            }.sheet(isPresented: $showResult, content: {Result(start: $start, end: $end)})
-            
+                VStack {
+                    Text("How to Go")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Image("Image")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100,height: 100)
+                }
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20.0)
+                        .fill(.gray)
+                        .opacity(0.3)
+                        .aspectRatio(contentMode: .fit)
+                    VStack {
+                        Text("สถานีเริ่มต้น")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        TextField("ถานีเริ่มต้น", value: $start, formatter: NumberFormatter())
+                            .textFieldStyle(.roundedBorder)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200)
+                        Text("สถานที่ปลายทาง")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        TextField("สถานที่ปลายทาง", value: $end, formatter: NumberFormatter())
+                            .textFieldStyle(.roundedBorder)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200)
+                        Button("Submit",action:{showResult.toggle()} )
+                            .padding([.top],40)
+                    }.sheet(isPresented: $showResult, content: {Result(start: $start, end: $end)})
+                    
+                }
+            }
+                    
         }
+        
+        
+        
     }
 }
 
